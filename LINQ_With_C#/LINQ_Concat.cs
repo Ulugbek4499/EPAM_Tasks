f95d117b58598fcccb28a8f7791901a9b782721f
Ulugbek4499 <ulugbektoshtemirov2000@gmail.com>
2023-08-27 23:54:46 +0500

    save

---
+++ LINQ_With_C#/LINQ_Concat.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_With_C_
+{
+    public class LINQ_Concat
+    {
+        // Concat it concatenate two sequences into one sequence. 
+        public static void StartUp()
+        {
+            List<int> sequence1 = new List<int> { 1, 2, 3, 4 };
+            List<int> sequence2 = new List<int> { 2, 4, 6, 8 };
+
+            var MS=sequence1.Concat(sequence2); //1 2 3 4 2 4 6 8
+
+            var QS = (from a in sequence1
+                      select a).Concat(sequence2);
+
+            foreach (var item in MS)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ LINQ_With_C#/LINQ_Ordering.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_With_C_
+{
+    public class LINQ_Ordering
+    {
+        //  OrderBy
+        //  OrderByDescending
+        //  ThenBy
+        //  ThenByDescending
+        //  Reverse
+        public static void StartUp()
+        {
+
+        }
+    }
+}
