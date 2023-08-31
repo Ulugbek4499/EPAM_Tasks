eec2a2dfffe6d6c9f104d80a2055d7f0c3fd777c
Ulugbek4499 <ulugbektoshtemirov2000@gmail.com>
2023-08-31 08:43:54 +0500

    save

---
+++ LINQ_With_C#/LINQ_Joins.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Buffers.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace LINQ_With_C_
+{
+    public class LINQ_Joins
+    {
+        //Join: This operator is used to join two data sources or collections based on common property and
+        //return the data as a single result set.
+
+        //GroupJoin: This operator is also used to join two data sources or collections based on a common key
+        //or property but returns the result as a group of sequences.
+    }
+}
